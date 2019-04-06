3a2e256a2d53e5a0f9c63693848b98fac44c4826
LULU <lucek111@gmail.com>
2019-04-06 22:28:02 +0200

    Update and rename patheDestroyerExperimental.py to patheDestroyerActiveDev.py

--- patheDestroyerActiveDev.py
+++ patheDestroyerActiveDev.py
@@ -10,15 +10,14 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1920,1080')
-#chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--headless')	
 chrome_options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
 #choose your seats
-seat = ['//*[@id="seatIdA28"]', '//*[@id="seatIdB28"]', '//*[@id="seatIdC28"]', '//*[@id="seatIdD28"]', '//*[@id="seatIdE28"]', '//*[@id="seatIdE26"]', '//*[@id="seatIdE24"]', '//*[@id="seatIdE22"]']
+seat = ['//*[@id="seatIdE15"]', '//*[@id="seatIdE16"]', '//*[@id="seatIdE17"]', '//*[@id="seatIdE18"]', '//*[@id="seatIdE19"]', '//*[@id="seatIdE20"]']
 #Link to movie booking
-bookingLink = "https://s.cinemaspathegaumont.com/#/C3380S203854/booking"
-
+bookingLink = "http://s.cinemaspathegaumont.com/#/C3132S50381/booking"
 
 
 driver.set_page_load_timeout(100)
@@ -26,17 +25,17 @@ print("Pathe destroyer is starting")
 driver.get(bookingLink)
 
 def find(driver):
-    element = driver.find_elements_by_id("seatIdF4")
+    element = driver.find_elements_by_xpath(seat[0])
     if element:
         return element
     else:
         return False
 
 def addPlace(placeNM):
-    element = WebDriverWait(driver, 30).until(find)
+    element = WebDriverWait(driver, 10).until(find)
     element = driver.find_element_by_xpath(placeNM)
     driver.execute_script("arguments[0].click();", element)
-    time.sleep(4)
+    time.sleep(2)
 
 
 print("One Seat Blocked")
@@ -51,65 +50,29 @@ print("Five Seats Blocked")
 addPlace(seat[4])
 print("Six Seats Blocked")
 addPlace(seat[5])
-print("Seven Seats Blocked")
-addPlace(seat[6])
-print("Eight Seats Blocked")
-addPlace(seat[7])
 
-
-#checkout
-def find1(driver):
-    element = driver.find_elements_by_xpath("/html/body/cgp-front-app/section/div[1]/div[4]/div[8]/div[2]")
-    if element:
-        return element
+def checkout(driver):
+    buttonBook = driver.find_elements_by_xpath("/html/body/cgp-front-app/section/div[1]/div[4]/div[8]/div[2]/a")
+    if buttonBook:
+        return buttonBook
     else:
         return False
 
-element = WebDriverWait(driver, 30).until(find1)
+buttonBook = WebDriverWait(driver, 10).until(checkout)
 element = driver.find_element_by_xpath("/html/body/cgp-front-app/section/div[1]/div[4]/div[8]/div[2]/a")
 print("Going to checkout")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
+time.sleep(2)
 
-
-def find2(driver):
-    element = driver.find_elements_by_xpath(
-        "/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
-    if element:
-        return element
-    else:
-        return False
-
-element = WebDriverWait(driver, 30).until(find2)
-element = driver.find_element_by_xpath(
-    "/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
+element = driver.find_element_by_xpath("/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
+time.sleep(2)
 
-def find3(driver):
-    element = driver.find_elements_by_xpath('//*[@id="popin-ajout-carte-cinepass"]/div[2]/div/div[1]/div/a')
-    if element:
-        return element
-    else:
-        return False
-
-element = WebDriverWait(driver, 30).until(find3)
 element = driver.find_element_by_xpath('//*[@id="popin-ajout-carte-cinepass"]/div[2]/div/div[1]/div/a')
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
-def find4(driver):
-    element = driver.find_elements_by_xpath("/html/body/section/div[2]/div/a[2]")
-    if element:
-        return element
-    else:
-        return False
-element = WebDriverWait(driver, 30).until(find4)
+time.sleep(2)
+
 element = driver.find_element_by_xpath("/html/body/section/div[2]/div/a[2]")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
-
 print("Pathe destroyer will run again in 10 minutes")
 driver.quit()
-
-
-
