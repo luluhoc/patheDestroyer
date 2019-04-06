448628b5b14ce6af66e7e1b932baa723cf9b174c
LULU <lucek111@gmail.com>
2019-04-06 21:55:23 +0200

    Update patheDestroyer.py

--- patheDestroyer.py
+++ patheDestroyer.py
@@ -20,7 +20,6 @@ seat = ['//*[@id="seatIdE15"]', '//*[@id="seatIdE16"]', '//*[@id="seatIdE17"]', 
 bookingLink = "http://s.cinemaspathegaumont.com/#/C3132S50381/booking"
 
 
-
 driver.set_page_load_timeout(100)
 print("Pathe destroyer is starting")
 driver.get(bookingLink)
@@ -33,7 +32,7 @@ def find(driver):
         return False
 
 def addPlace(placeNM):
-    element = WebDriverWait(driver, 30).until(find)
+    element = WebDriverWait(driver, 10).until(find)
     element = driver.find_element_by_xpath(placeNM)
     driver.execute_script("arguments[0].click();", element)
     time.sleep(2)
@@ -52,60 +51,21 @@ addPlace(seat[4])
 print("Six Seats Blocked")
 addPlace(seat[5])
 
-
-def find1(driver):
-    element = driver.find_elements_by_xpath("/html/body/cgp-front-app/section/div[1]/div[4]/div[8]/div[2]")
-    if element:
-        return element
-    else:
-        return False
-
-
-element = WebDriverWait(driver, 30).until(find1)
 element = driver.find_element_by_xpath("/html/body/cgp-front-app/section/div[1]/div[4]/div[8]/div[2]/a")
 print("Going to checkout")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
+time.sleep(2)
 
-
-def find2(driver):
-    element = driver.find_elements_by_xpath(
-        "/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
-    if element:
-        return element
-    else:
-        return False
-
-
-element = WebDriverWait(driver, 30).until(find2)
-element = driver.find_element_by_xpath(
-    "/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
+element = driver.find_element_by_xpath("/html/body/cgp-front-app/section/div[1]/div[3]/div[1]/div[1]/div[1]/div/div[2]/button")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
+time.sleep(2)
 
-
-def find3(driver):
-    element = driver.find_elements_by_xpath('//*[@id="popin-ajout-carte-cinepass"]/div[2]/div/div[1]/div/a')
-    if element:
-        return element
-    else:
-        return False
-
-element = WebDriverWait(driver, 30).until(find3)
 element = driver.find_element_by_xpath('//*[@id="popin-ajout-carte-cinepass"]/div[2]/div/div[1]/div/a')
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
-def find4(driver):
-    element = driver.find_elements_by_xpath("/html/body/section/div[2]/div/a[2]")
-    if element:
-        return element
-    else:
-        return False
-element = WebDriverWait(driver, 30).until(find4)
+time.sleep(2)
+
 element = driver.find_element_by_xpath("/html/body/section/div[2]/div/a[2]")
 driver.execute_script("arguments[0].click();", element)
-time.sleep(1)
-
 print("Pathe destroyer will run again in 10 minutes")
 driver.quit()
 
